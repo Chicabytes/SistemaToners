232e6489b9b1a0ea9dd54fa6646f610d787a8a1c
Chicabytes <chocobarjulietain@gmail.com>
2021-08-18 12:54:37 -0300

    arreglo de formulario

--- SistemaToners/Form1.cs
+++ SistemaToners/Form1.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SistemaToners
 {
-    public partial class Form1 : Form
+    public partial class Sistoners : Form
     {
-        public Form1()
+        public Sistoners()
         {
             InitializeComponent();
         }
