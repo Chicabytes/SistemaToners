4769bb4f5605a56e85bb425974d1de28b0eb233d
Chicabytes <chocobarjulietain@gmail.com>
2021-08-19 12:55:56 -0300

    agrega cambios y formulario

---
+++ SistemaToners/NuevoToner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaToners
+{
+    public partial class NuevoToner : Form
+    {
+        public NuevoToner()
+        {
+            InitializeComponent();
+        }
+    }
+}
