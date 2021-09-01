d7fbe19524acdfc9cc3948a416203210df7e469e
Chicabytes <chocobarjulietain@gmail.com>
2021-09-01 15:33:46 -0300

    Se agrega lista de Areas al archivo de conexiones

--- SistemaToners/Conexion/Conexiones.cs
+++ SistemaToners/Conexion/Conexiones.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
 using SistemaToners.Entidades;
+using System;
 
 namespace SistemaToners.Conexion
 {
@@ -19,6 +20,27 @@ namespace SistemaToners.Conexion
             command.ExecuteNonQuery();
             conexion.Close();
         }
-        
+        public List<Area> ListaAreas()
+        {
+            Area _area = new Area();
+            List<Area> ListaArea = new List<Area>();
+            string path = Path.GetFullPath("SistemaToners.db");
+            string cadena = "Data Source=" + path;
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Select * From Area";
+            command.ExecuteNonQuery();
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                _area.Id = Convert.ToInt32(reader["idArea"]);
+                _area.Nombre_area = reader["nombreArea"].ToString();
+                ListaArea.Add(_area);
+            }
+            reader.Close();
+            conexion.Close();
+            return ListaArea;
+        }
     }
 }
